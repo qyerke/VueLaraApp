344eb456ffb0ec906456d0508a1f2e71e714ea92
Yerke Nurgali <yerke.q@gmail.com>
2020-01-03 00:41:37 +0600

    add vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,18 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+  { path: '/dashboard', component: require('./components/DashboardComponent.vue').default},
+  { path: '/profile', component: require('./components/ProfileComponent.vue').default}
+]
+ 
+
+const router = new VueRouter({
+  routes
+})
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +32,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('dashboard', require('./components/DashboardComponent.vue').default);
+// Vue.component('profile', require('./components/ProfileComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +43,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
